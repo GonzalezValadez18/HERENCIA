7d61d5fe2b26caf09ca7fef7afa7b8d5f12a14b8
Jose Leonardo Gonzalez Valadez <62527201+GonzalezValadez18@users.noreply.github.com>
2020-04-05 22:23:06 -0500

    Add files via upload
    CLASES QUE FORMAN EL PROGRAMA HERENCIA

---
+++ Dog.java
@@ -0,0 +1,51 @@
+package herencia6;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Angel
+ */
+public class Dog {
+    private String name;
+	private double weight;
+	private String colour;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public void printToConsole() {
+		System.out.print(" Nombre: " + getName());
+		System.out.print(" color: " + this.colour);
+		System.out.println(" El peso: " + this.weight);
+	}
+}
+
+ 
+
+
